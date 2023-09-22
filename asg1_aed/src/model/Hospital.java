2a9f38cc3af22c3d91f045e5242602e488478e4d
SejalSatav <145803361+SejalSatav@users.noreply.github.com>
2023-09-22 15:19:56 -0400

    Add files via upload

---
+++ asg1_aed/src/model/Hospital.java
@@ -0,0 +1,366 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hp
+ */
+public class Hospital {
+    
+    private String name;
+    private String age;
+    private String dob;
+    private String gender;
+    private String country;
+    private String mobnum;
+    private String bldgrp;
+    private String ssnnum;
+    private String web;
+    private String email;
+    private String gpsloc;
+    private String city;
+    private String state;
+    private String country2;
+    private String carlicnum;
+    private String issuedate;
+    private String validdate;
+    private String model;
+    private String serialnum;
+    private String numplate;
+    private String chassisnum;
+    private String patid;
+    private String primdoc;
+    private String lastvisit;
+    private String allergy;
+    private String med;
+    private String nextvisit;
+    private String docname;
+    private String doclicnum;
+    private String speciality;
+    private String degree;
+    private String doccontnum;
+    private String officeadd;
+    private String officetime;
+    private String corpid;
+    private String insuradd;
+    private String premium;
+    private String dedctamt;
+    private String benefits;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getMobnum() {
+        return mobnum;
+    }
+
+    public void setMobnum(String mobnum) {
+        this.mobnum = mobnum;
+    }
+
+    public String getBldgrp() {
+        return bldgrp;
+    }
+
+    public void setBldgrp(String bldgrp) {
+        this.bldgrp = bldgrp;
+    }
+
+    public String getSsnnum() {
+        return ssnnum;
+    }
+
+    public void setSsnnum(String ssnnum) {
+        this.ssnnum = ssnnum;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGpsloc() {
+        return gpsloc;
+    }
+
+    public void setGpsloc(String gpsloc) {
+        this.gpsloc = gpsloc;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry2() {
+        return country2;
+    }
+
+    public void setCountry2(String country2) {
+        this.country2 = country2;
+    }
+
+    public String getCarlicnum() {
+        return carlicnum;
+    }
+
+    public void setCarlicnum(String carlicnum) {
+        this.carlicnum = carlicnum;
+    }
+
+    public String getIssuedate() {
+        return issuedate;
+    }
+
+    public void setIssuedate(String issuedate) {
+        this.issuedate = issuedate;
+    }
+
+    public String getValiddate() {
+        return validdate;
+    }
+
+    public void setValiddate(String validdate) {
+        this.validdate = validdate;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialnum() {
+        return serialnum;
+    }
+
+    public void setSerialnum(String serialnum) {
+        this.serialnum = serialnum;
+    }
+
+    public String getNumplate() {
+        return numplate;
+    }
+
+    public void setNumplate(String numplate) {
+        this.numplate = numplate;
+    }
+
+    public String getChassisnum() {
+        return chassisnum;
+    }
+
+    public void setChassisnum(String chassisnum) {
+        this.chassisnum = chassisnum;
+    }
+
+    public String getPatid() {
+        return patid;
+    }
+
+    public void setPatid(String patid) {
+        this.patid = patid;
+    }
+
+    public String getPrimdoc() {
+        return primdoc;
+    }
+
+    public void setPrimdoc(String primdoc) {
+        this.primdoc = primdoc;
+    }
+
+    public String getLastvisit() {
+        return lastvisit;
+    }
+
+    public void setLastvisit(String lastvisit) {
+        this.lastvisit = lastvisit;
+    }
+
+    public String getAllergy() {
+        return allergy;
+    }
+
+    public void setAllergy(String allergy) {
+        this.allergy = allergy;
+    }
+
+    public String getMed() {
+        return med;
+    }
+
+    public void setMed(String med) {
+        this.med = med;
+    }
+
+    public String getNextvisit() {
+        return nextvisit;
+    }
+
+    public void setNextvisit(String nextvisit) {
+        this.nextvisit = nextvisit;
+    }
+
+    public String getDocname() {
+        return docname;
+    }
+
+    public void setDocname(String docname) {
+        this.docname = docname;
+    }
+
+    public String getDoclicnum() {
+        return doclicnum;
+    }
+
+    public void setDoclicnum(String doclicnum) {
+        this.doclicnum = doclicnum;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getDoccontnum() {
+        return doccontnum;
+    }
+
+    public void setDoccontnum(String doccontnum) {
+        this.doccontnum = doccontnum;
+    }
+
+    public String getOfficeadd() {
+        return officeadd;
+    }
+
+    public void setOfficeadd(String officeadd) {
+        this.officeadd = officeadd;
+    }
+
+    public String getOfficetime() {
+        return officetime;
+    }
+
+    public void setOfficetime(String officetime) {
+        this.officetime = officetime;
+    }
+
+    public String getCorpid() {
+        return corpid;
+    }
+
+    public void setCorpid(String corpid) {
+        this.corpid = corpid;
+    }
+
+    public String getInsuradd() {
+        return insuradd;
+    }
+
+    public void setInsuradd(String insuradd) {
+        this.insuradd = insuradd;
+    }
+
+    public String getPremium() {
+        return premium;
+    }
+
+    public void setPremium(String premium) {
+        this.premium = premium;
+    }
+
+    public String getDedctamt() {
+        return dedctamt;
+    }
+
+    public void setDedctamt(String dedctamt) {
+        this.dedctamt = dedctamt;
+    }
+
+    public String getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(String benefits) {
+        this.benefits = benefits;
+    }
+
+    
+}
